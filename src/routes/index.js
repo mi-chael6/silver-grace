264ca5a86658dc9f7d119d8fe1eb01450664dc3e
Michael <melomb@students.nu-moa.edu.ph>
2025-10-18 05:10:18 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "../pages/home/components/Homepage";
 import ProductListing from "../pages/product_listing/components/ProductListing";
 import Product_Detail from "../pages/product_detail/components/Product_Detail";
 import Contact from "../pages/contact/components/Contact";
 import About from "../pages/about_us/components/About";
 
-
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/home" replace />} />
+
       <Route path="/home" element={<Homepage />} />
       <Route path="/shop" element={<ProductListing />} />
       <Route path="/contact" element={<Contact />} />
